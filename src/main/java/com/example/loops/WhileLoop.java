526f5fc3325e85ad59656da1f42c0d5e89476640
Don Kolony <donkolony@gmail.com>
2026-04-25 13:24:47 +0200

    feat(loops): implement while loop countdown using do-while loop

--- src/main/java/com/example/loops/WhileLoop.java
+++ src/main/java/com/example/loops/WhileLoop.java
@@ -29,3 +29,16 @@ public class WhileLoop {
         System.out.println("Liftoff!");
     }
 }
+
+//public class WhileLoop {
+//    public static void main(String[] args) {
+//        // Write your code here
+//
+//        int i = 5;
+//
+//        do {
+//            System.out.println(i);
+//            i--;
+//        } while (i>0);
+//    }
+//}
---
+++ src/test/java/com/example/methods/BasicMethods.java
@@ -0,0 +1,4 @@
+package com.example.methods;
+
+public class BasicMethods {
+}
