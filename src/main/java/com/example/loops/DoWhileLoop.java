bff705d71221ca42cb94e49cf7635da401dabe65
Don Kolony <donkolony@gmail.com>
2026-04-26 16:26:46 +0200

    feat(loops): implement do while loop count down

--- src/main/java/com/example/loops/DoWhileLoop.java
+++ src/main/java/com/example/loops/DoWhileLoop.java
@@ -1,4 +1,13 @@
 package com.example.loops;
 
 public class DoWhileLoop {
+    public static void main(String[] args) {
+        // Write your code here
+        int i = 5;
+
+        do {
+            System.out.println(i);
+            i--;
+        } while (i>0);
+    }
 }
